9cb85b138a76e9a6378646a05b9c1c2bb863d344
franshap <frances.sari@gmail.com>
2015-04-20 18:15:38 -0400

    fixed pmcc
    still not 100% sure but on my way to finishing

--- stat_calc.py
+++ stat_calc.py
@@ -6,7 +6,6 @@ class stat_calc:
 	def mean_matrix(self, matrix) :
 		' This calculates the mean of a variance vector VC or VP'
 		return sum(matrix) * 1.0 / len(matrix)
-		'question: will the matrix have more than one column?'
 
 	def variation_helper(self) :
 		'calls mean_matrix on VC and VP and gives variation'
@@ -23,7 +22,7 @@ class stat_calc:
 		' unbiased is a bool, if true then (1/n-1) if false (1/n) '
 		N = len(lst)
 		avg = sum(lst) * 1.0 / N
-		epsilon = reduce(lambda x: (x - avg)**2, lst)
+		epsilon = map(lambda x: (x - avg)**2, lst)
 
 		if unbiased:  
 			variance = sum(epsilon) * 1.0 / N - 1 
@@ -35,8 +34,17 @@ class stat_calc:
 
 	def pmcc (lst1, lst2) :
 		' Takes in two lists, calculates the correlation coefficient between them'
-		's1 = math.sqrt(variance(lst1))'
-		's2 = math.sqrt(variance(lst2))'
+		s1 = math.sqrt(variance(lst1))
+		s2 = math.sqrt(variance(lst2))
+		N = len(lst1)
+		avg1 = sum(lst1) * 1.0 / N
+		avg2 = sum(lst2) * 1.0 / N
+		epsilon = map(lambda x, y: (x - avg1)*(y - avg2), lst1, lst2)
+ 		covariance = sum(epsilon) * 1.0 / N - 1 
+		
+		return (s1*s2)/covariance
+
+
 		'question: should this output a list or a value?'
 
 
