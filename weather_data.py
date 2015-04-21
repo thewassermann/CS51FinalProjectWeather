a2f0ae2b36270f4f320efdadd5174e7598b1a0d2
mcinnamon1 <mcinnamon4@MOLLYs-MacBook-Pro.local>
2015-04-21 13:59:52 -0400

    Used Numpy to extract data from CSV

--- weather_data.py
+++ weather_data.py
@@ -1,15 +1,20 @@
-'import a .csv file'
+import numpy
 
 class weather_data:
 
+	CD_matrix = numpy.empty([9,5])
+	PD_matrix = numpy.empty([16, 5])
+
 	def make_CD (self, csv) :
-		'create matrix of necessary information'
+		CD_matrix = numpy.genfromtxt('CD.csv', delimiter=',')
 
 	def make_PD (self, csv) :
-		'create matrix of necessary information'
+		PD_matrix = numpy.genfromtxt('PD.csv', delimiter=',')
 
 	def get_PD (self) :
-		'expose PD'
+		self.make_CD
+		return PD_matrix
 
 	def get_CD (self) :
-		'expose CD'
+		self.make_PD
+		return CD_matrix
