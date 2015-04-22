6359dd56e4ad1e05d3271ba9a97da2e9233246cd
granthoechst <granthoechst@college.harvard.edu>
2015-04-22 15:52:17 -0400

    First stab at the sliding windows

--- main_algorithm.py
+++ main_algorithm.py
@@ -1,10 +1,16 @@
 'bulk of the algorithm is computed here'
 from weather_data import get_CD, get_PD
+import numpy as np
 class main_algorithm:
 
 	def sliding_windows (self, PD) :
 		'uses iterative process to create 8 sliding windows from PD'
 		'present them as a list'
+		# Ok here's how we'll do this
+		# PD should be in the form of a 14 x 4 matrix already SO
+		W = []
+		For i in range (0, 8)
+			W.append(np.vstack((PD[i],PD[i+1],PD[i+2],PD[i+3],PD[i+4],PD[i+5],PD[i+6])))
 
 	def euc_distance (self, CD) :
 		'uses iterative process to compute euclidian distance of each sliding window with CD'
