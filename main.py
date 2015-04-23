678b8f8da6e0eb78b4628bb3cb695745c2cf40a3
mcinnamon1 <mcinnamon4@MOLLYs-MacBook-Pro.local>
2015-04-23 10:45:15 -0400

    Printing Works!!!

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from stat_calc import get_variation
-from weather_data import prev_day
-class main:
+# from stat_calc import get_variation
+# from weather_data import prev_day
+# class main:
 
-	def finishing_move (sef, get_variation, prev_day):
-			'add variation and prev_day conditions'
+# 	def finishing_move (sef, get_variation, prev_day):
+# 			'add variation and prev_day conditions'
+
+from weather_data import WeatherData
+
+w = WeatherData()
+
+a = w.make_CD('CD.csv')
+b = w.make_PD('PD.csv')
+
+print a
+print b
--- weather_data.py
+++ weather_data.py
@@ -1,20 +1,18 @@
 import numpy
 
-class weather_data:
+class WeatherData:        
 
-	CD_matrix = numpy.empty([9,5])
-	PD_matrix = numpy.empty([16, 5])
+	# CD_matrix = numpy.empty([9,5])
+	# PD_matrix = numpy.empty([16, 5])
 
 	def make_CD (self, csv) :
-		CD_matrix = numpy.genfromtxt('CD.csv', delimiter=',')
+		return numpy.genfromtxt(csv, delimiter=',')
 
 	def make_PD (self, csv) :
-		PD_matrix = numpy.genfromtxt('PD.csv', delimiter=',')
+		return numpy.genfromtxt(csv, delimiter=',')
 
 	def get_PD (self) :
-		self.make_CD
-		return PD_matrix
+		return self.make_PD
 
 	def get_CD (self) :
-		self.make_PD
-		return CD_matrix
+		return self.make_CD
